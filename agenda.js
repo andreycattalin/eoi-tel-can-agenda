15f9c79aac182e3937e99f2e03e0fe3d2ef2e700
andrey <cattalin.htc@gmail.com>
2024-12-19 20:41:07 +0100

    --wip-- [skip ci]

--- agenda.js
+++ agenda.js
@@ -1,3 +1,5 @@
+const contactList = document.getElementById("contactList")
+
 async function getContacts() {
     const token = getToken()
 
@@ -22,6 +24,7 @@ async function getContacts() {
         const data = await response.json();
 
         console.log(data)
+        printContacts(data)
     } catch (error) {
         console.error(error);
     }
@@ -35,6 +38,23 @@ function getToken() {
 function getUserId() {
     const user = localStorage.getItem(STORAGE_USER)
     return JSON.parse(user).user.id
+}
+
+function printContacts(contacts) {
+
+    let html = ""
+    contacts.forEach(contact => {
+        html += `<div class="w-full shadow-md shadow-slate-200 bg-slate-50 rounded-lg px-4 py-2">
+                <h4 class="font-bold">${contact.name}</h4>
+                <div class="mt-1">
+                    <p class="text-sm bg-blue-500 text-white w-fit px-2 py-0.5 rounded-lg">${contact.email}</p>
+                    <p class="text-sm mt-2 border-t pt-2 border-dashed">${contact.phone}</p>
+                </div>
+            </div>`
+    });
+
+    contactList.innerHTML = html
+
 }
 
 
