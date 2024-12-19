8214a48d68c03d7d411b0d925bd8285e1c4ef84d
andrey <cattalin.htc@gmail.com>
2024-12-19 18:24:51 +0100

    --wip-- [skip ci]

--- main.js
+++ main.js
@@ -38,6 +38,51 @@ async function login() {
         setLoading(false)
         // Redirect to app
 
+        window.location.href = "/agenda.html"
+
+    } catch (error) {
+        setLoading(false)
+        console.error(error);
+    }
+}
+
+async function register() {
+    setLoading(true)
+
+    const bodyData = JSON.stringify({
+        "email": email.value,
+        "password": pass.value
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "apiKey": API_KEY
+        },
+        body: bodyData
+    };
+
+    try {
+        const response = await fetch(endpoints.register, requestOptions)
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+
+        if (data.access_token) {
+            localStorage.setItem(STORAGE_USER, JSON.stringify(data.user));
+        }
+
+        console.log("User logged in successfully");
+
+        setLoading(false)
+        // Redirect to app
+
+        window.location.href = "/agenda.html"
+
     } catch (error) {
         setLoading(false)
         console.error(error);
--- shared.js
+++ shared.js
@@ -1,6 +1,10 @@
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyaWxlb2ZkamtjbXNwdmVibnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzk1MDYsImV4cCI6MjA0OTg1NTUwNn0.MZx5Cpcw6aqM7A9Sc8_VC6HWnSKQ0SYkWpTqUAI0-Pg"
 const STORAGE_USER = "user"
 
+const BASE_URL = "https://irileofdjkcmspvebnqq.supabase.co/auth/v1"
+
 const endpoints = {
-    login: "https://irileofdjkcmspvebnqq.supabase.co/auth/v1/token?grant_type=password"
+    login: `${BASE_URL}/token?grant_type=password`,
+    register: `${BASE_URL}/signup`,
+    contact: "todo"
 }
